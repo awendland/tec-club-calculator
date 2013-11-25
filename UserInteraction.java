ba5cfa634ce86cf04ff8359317e35ef45d776f9c
Alex Wendland <me@alexwendland.com>
2013-11-25 15:01:29 -0800

    Allow user to run multiple calculations without restarting the program.

--- UserInteraction.java
+++ UserInteraction.java
@@ -15,63 +15,72 @@ public class UserInteraction
         // Initialize Scanner for taking user input
         Scanner in = new Scanner(System.in);
 
-        // Display a request for user input
-        print("Would you like to add, subtract, multiply, or divide?\na/s/m/d: ");
-
-        // Wait on user input
-        String ui = in.next();
-        
-        /* Following DRY-coding:
-         * because all of the Calculator's functions require two numbers to be inputted,
-         * this program shall ask for the input before the if-statements so that each
-         * statement can simply use these values instead of having to ask for it in each
-         * if-block.
+        /* Setup an infinite loop so that the user may run multiple calculationswithout
+         * restarting this program
          */
-        print("First Number: ");
-        double a = in.nextDouble();
-        print("Second Number: ");
-        double b = in.nextDouble();
+        while(true) {
 
-        // Select course of action based on user input
-        if (ui.equals("a")) {
-            double result = calc.add(a, b);
-            println(
-                String.valueOf(a) +
-                " + " +
-                String.valueOf(b) +
-                " = " +
-                String.valueOf(result)
-            );
-        } else if (ui.equals("s")) {
-            double result = calc.subtract(a, b);
-            println(
-                String.valueOf(a) +
-                " - " +
-                String.valueOf(b) +
-                " = " +
-                String.valueOf(result)
-            );
-        } else if (ui.equals("m")) {
-            double result = calc.multiply(a, b);
-            println(
-                String.valueOf(a) +
-                " * " +
-                String.valueOf(b) +
-                " = " +
-                String.valueOf(result)
-            );
-        } else if (ui.equals("d")) {
-            double result = calc.divide(a, b);
-            println(
-                String.valueOf(a) +
-                " / " +
-                String.valueOf(b) +
-                " = " +
-                String.valueOf(result)
-            );
+            // Display a request for user input
+            print("Would you like to add, subtract, multiply, or divide?\na/s/m/d: ");
+
+            // Wait on user input
+            String ui = in.next();
+
+            /* Following DRY-coding:
+             * because all of the Calculator's functions require two numbers to be inputted,
+             * this program shall ask for the input before the if-statements so that each
+             * statement can simply use these values instead of having to ask for it in each
+             * if-block.
+             */
+            print("First Number: ");
+            double a = in.nextDouble();
+            print("Second Number: ");
+            double b = in.nextDouble();
+
+            // Select course of action based on user input
+            if (ui.equals("a")) {
+                double result = calc.add(a, b);
+                println(
+                    String.valueOf(a) +
+                    " + " +
+                    String.valueOf(b) +
+                    " = " +
+                    String.valueOf(result)
+                );
+            } else if (ui.equals("s")) {
+                double result = calc.subtract(a, b);
+                println(
+                    String.valueOf(a) +
+                    " - " +
+                    String.valueOf(b) +
+                    " = " +
+                    String.valueOf(result)
+                );
+            } else if (ui.equals("m")) {
+                double result = calc.multiply(a, b);
+                println(
+                    String.valueOf(a) +
+                    " * " +
+                    String.valueOf(b) +
+                    " = " +
+                    String.valueOf(result)
+                );
+            } else if (ui.equals("d")) {
+                double result = calc.divide(a, b);
+                println(
+                    String.valueOf(a) +
+                    " / " +
+                    String.valueOf(b) +
+                    " = " +
+                    String.valueOf(result)
+                );
+            }
+            
+            // Print a newline at the end of each loop to provide display clarity
+            println("");
         }
     }
-    
+
     /**
      * Convenience wrapper around the {@link System.out.print()} method. Prints a string form of the object.
      * 
